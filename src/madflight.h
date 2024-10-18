dca644d9af364ca57a7d251c2e8546862d7471aa
qqqlab <46283638+qqqlab@users.noreply.github.com>
2024-10-18 15:06:30 +0200

    add sensors to i2c scanner

--- src/madflight.h
+++ src/madflight.h
@@ -1,4 +1,4 @@
-#define MADFLIGHT_VERSION "madflight v1.2.0"
+#define MADFLIGHT_VERSION "madflight v1.2.1-DEV"
 
 /*==========================================================================================
 madflight - Flight Controller for ESP32 / RP2040 / STM32
