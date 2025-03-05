a7dc113051f0012b143046b23782103f9e9900e2
qqqlab <46283638+qqqlab@users.noreply.github.com>
2025-03-05 10:42:59 +0100

    v1.3.3

--- src/madflight.h
+++ src/madflight.h
@@ -1,4 +1,4 @@
-#define MADFLIGHT_VERSION "madflight v1.3.3-DEV"
+#define MADFLIGHT_VERSION "madflight v1.3.3"
 
 /*==========================================================================================
 madflight - Flight Controller for ESP32 / ESP32-S3 / RP2350 / RP2040 / STM32
