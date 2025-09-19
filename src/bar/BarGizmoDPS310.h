4b15ea917a3e49c9d49390af22bb33779a559fdd
Enzo PB <enzo.puiroux@mail.com>
2025-09-19 21:21:05 +0200

    make bar measurement continuous (wip)

--- src/bar/BarGizmoDPS310.h
+++ src/bar/BarGizmoDPS310.h
@@ -10,24 +10,22 @@ protected:
 public:
   BarGizmoDPS310(MF_I2C *i2c, int8_t i2c_adr, uint32_t sampleRate) {
     if (i2c_adr == 0) {
-      i2c_adr = 0x77;
+      i2c_adr = 0x77;  // default i2c address for the DPS310
     }
     pressureSensor.begin(*i2c, i2c_adr);
+    int16_t ret = pressureSensor.startMeasureBothCont(
+      DPS__MEASUREMENT_RATE_128, DPS__OVERSAMPLING_RATE_2,
+      DPS__MEASUREMENT_RATE_128, DPS__OVERSAMPLING_RATE_2
+    );
+    // TODO: find a way to make gizmo check fail (bar.cpp line 90)
+    if (ret != 0) {
+      Serial.println("bar init start measure cont failed");
+      while (1);
+    }
   }
 
   bool update(float *press, float *temp) override {
-    int16_t ret;
-
-    ret = pressureSensor.measurePressureOnce(*press);
-    if (ret != 0) {
-      return false;
-    }
-
-    ret = pressureSensor.measureTempOnce(*temp);
-    if (ret != 0) {
-      return false;
-    }
-
-    return true;
+    uint8_t measureCount = 1;
+    return pressureSensor.getContResults(temp, measureCount, press, measureCount) == 0;
   }
 };
